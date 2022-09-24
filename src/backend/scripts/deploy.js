b8a623fc7fd93d57a54b3a16956c9d3d9f568b66
atasen19 <atasen19@itu.edu.tr>
2022-09-24 03:20:09 +0300

    added NFT smart contract

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,3 +1,5 @@
+const { ethers } = require("hardhat");
+
 async function main() {
 
   const [deployer] = await ethers.getSigners();
@@ -6,10 +8,12 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // deploy contracts here:
+  const NFT = await ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy();
   
-  
+  console.log("NFT contract address", nft.address)
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  saveFrontendFiles(nft, "NFT");
 }
 
 function saveFrontendFiles(contract, name) {
